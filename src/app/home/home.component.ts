2e1a9396a8db09b9e4ace4dd6a98e834442ebd31
jefrwld <jeffrey.freiwald98@gmail.com>
2025-04-30 18:03:48 +0200

    some changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {CommonModule, NgForOf} from '@angular/common';
+
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  recentProjects = ['Study Budy', 'Swipedia']; 
 
 }
