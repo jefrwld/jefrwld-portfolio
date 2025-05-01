3a6f71d7eedac52cc54e00951b09a95a2250d208
jefrwld <jeffrey.freiwald98@gmail.com>
2025-05-01 13:41:35 +0200

    new component navbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {CommonModule, NgForOf} from '@angular/common';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, NavBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit, } from '@angular/core';
 import { ProjectService } from '../project.service';
 import {Project} from '../models/project.model';
 import { CommonModule } from '@angular/common';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css'],
-  imports: [CommonModule],
+  imports: [CommonModule, NavBarComponent],
 })
 export class ProjectsComponent implements OnInit {
   projects: Project[] = [];
